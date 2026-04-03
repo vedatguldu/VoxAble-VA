f37c2a2da178073a67de2a2bfa53fda8220ca474
vedatguldu <vedatguldu29@gmail.com>
2026-04-03 17:42:43 +0300

    fix(build): root plugin tanimlarini explicit id/version yap

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.kotlin.kapt) apply false
-    alias(libs.plugins.hilt.android) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.google.services) apply false
-    alias(libs.plugins.ksp) apply false
+    id("com.android.application") version "8.5.2" apply false
+    id("com.android.library") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.10" apply false
+    id("org.jetbrains.kotlin.kapt") version "2.0.10" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.10" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.devtools.ksp") version "2.0.10-1.0.24" apply false
 }
